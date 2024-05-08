39f570fae8c071a863521cd0fa8ac77b59be371e
GeneWu <495612021@qq.com>
2024-05-08 23:11:53 +0800

    upd. update params.

--- detr/datasets/coco.py
+++ detr/datasets/coco.py
@@ -148,6 +148,10 @@ def build(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
+    # PATHS = {
+    #     "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
+    #     "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+    # }
     PATHS = {
         "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
         "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
